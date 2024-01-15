51ca1767951b1dcc84fbaefd440fd8d621c3ff33
jeyabdo <joephoenix755@gmail.com>
2024-01-15 21:37:52 +0000

    added logic.js file and modified html, css and js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,21 @@
-console.log(this)
+var questions = [
+    {
+      question: "What does HTML stand for?",
+      choices: ["Hyper Text Markup Language", "Hyperlink and Text Markup Language", "Hyper Transfer Markup Language", "HyperText Markup Language"],
+      correctAnswer: "HyperText Markup Language",
+    },
+    {
+      question: "Which of the following is used for styling in web development?",
+      choices: ["HTML", "CSS", "JavaScript", "Python"],
+      correctAnswer: "CSS",
+    },
+    {
+      question: "What is the purpose of JavaScript in web development?",
+      choices: ["1. Backend development", "2.Styling", "3. Frontend development", "4. Database management"],
+      correctAnswer: "Frontend development",
+    },
+    {
 
-// Create a code quiz that contains the following requirements:
-
-// A start button that when clicked a timer starts and the first question appears.
-
-// Questions contain buttons for each answer.
-// When answer is clicked, the next question appears
-// If the answer clicked was incorrect then subtract time from the clock
-// The quiz should end when all questions are answered or the timer reaches 0.
-
-// When the game ends, it should display their score and give the user the ability to save their initials and their score
+    }
+    // Add more questions with the same structure
+  ];
